72f111f2ab4f7cfeced3b80d4dd3d01db30d01a6
leejiwonn <bbongwa@naver.com>
2021-05-11 01:39:17 +0900

    chore: 기본 개발 환경셋팅 - `CRA` 사용

---
+++ src/app.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import resetCss from "reset-css";
+import { createGlobalStyle } from "styled-components";
+import MainPage from "./pages/MainPage";
+
+const GlobalStyle = createGlobalStyle`
+  ${resetCss};
+  html, body {
+    height: 100%;
+  }
+  #root {
+    width: 100%;
+    height: 100%;
+    font-family: 'Spoqa Han Sans', sans-serif;
+    font-weight: normal;
+  }
+	button {
+		border: none;
+		padding: 0;
+    font-family: 'Spoqa Han Sans', sans-serif;
+	}
+  ::-webkit-scrollbar {
+    display: none;
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
+  }
+`;
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={MainPage} />
+        </Switch>
+      </BrowserRouter>
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
