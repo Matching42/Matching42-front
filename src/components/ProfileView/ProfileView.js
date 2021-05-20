0ef7078433a8a30d45dd132817b8c3939b23e753
from97 <skm1104@daum.net>
2021-05-21 03:21:51 +0900

    feature: ProfileView 컴포넌트 틀 구현 및 스타일링
    - Title, Description, UserStateList 컴포넌트 생성 및 배치
    - Styled Component 사용해 컴포넌트 스타일링

--- src/components/ProfileView/ProfileView.js
+++ src/components/ProfileView/ProfileView.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { ProfileViewStyled } from './ProfileView.styles';
+import { ProfileViewStyled, Title, Description, UserStateList, UserStateItem } from './ProfileView.styles';
 
-const ProfileView = () => <ProfileViewStyled>ProfileView</ProfileViewStyled>;
+const ProfileView = () => 
+    <ProfileViewStyled>
+        <Title>Hello, seomoon !</Title>
+        <Description>오늘 하루도 힘차게 달려볼까요?<br/>오른쪽 상단의 매칭 신청하기 버튼을 눌러 시작해보세요!</Description>
+        <UserStateList>
+            <UserStateItem>Level: 2 - 30%</UserStateItem>
+            <UserStateItem>Black Hole : 106 days left</UserStateItem>
+        </UserStateList>
+    </ProfileViewStyled>;
 
 export default ProfileView;
