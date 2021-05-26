e6180c6947fa90ca745bc22250fc4f7ef6e1c883
from97 <skm1104@daum.net>
2021-05-26 15:01:29 +0900

    feature: MatchingStateView 컴포넌트 구조 작성

--- src/components/MatchingStateView/MatchingStateView.js
+++ src/components/MatchingStateView/MatchingStateView.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { MatchingStateViewStyled } from './MatchingStateView.styles';
+import { MatchingStateViewStyled, MatchingState, MatchingStartButton } from './MatchingStateView.styles';
 
-const MatchingStateView = () => <MatchingStateViewStyled>MatchingStateView</MatchingStateViewStyled>;
+const MatchingStateView = () => 
+    <MatchingStateViewStyled>
+        <MatchingState>현재 30명이 매칭을 기다리고 있어요!</MatchingState>
+        <MatchingStartButton>매칭 신청하기 +</MatchingStartButton>
+    </MatchingStateViewStyled>;
 
 export default MatchingStateView;
