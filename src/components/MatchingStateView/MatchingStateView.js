13f69530a42160d03d35c4eaf615667ba5cc75df
from97 <skm1104@daum.net>
2021-05-26 17:13:35 +0900

    feature: MatchingStateView 컴포넌트 props 정의
    - MatchingStateView 컴포넌트 default props 정의
    - MatchingStateView 컴포넌트에서 default props 사용

--- src/components/MatchingStateView/MatchingStateView.js
+++ src/components/MatchingStateView/MatchingStateView.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { scryRenderedDOMComponentsWithClass } from 'react-dom/test-utils';
 import { MatchingStateViewStyled, MatchingState, MatchingStartButton } from './MatchingStateView.styles';
 
-const MatchingStateView = () => 
+const MatchingStateView = (props) => 
     <MatchingStateViewStyled>
-        <MatchingState>현재 30명이 매칭을 기다리고 있어요!</MatchingState>
-        <MatchingStartButton>매칭 신청하기 +</MatchingStartButton>
+        <MatchingState>
+            현재 {props.waitList.size}명이 매칭을 기다리고 있어요!
+        </MatchingState>
+        <MatchingStartButton>
+            {props.user.waitMatching ? '매칭 대기 중' : '매칭 신청하기 +'}
+        </MatchingStartButton>
     </MatchingStateViewStyled>;
+
+MatchingStateView.defaultProps = {
+    waitList: {
+        size: 30,
+        cub3d: ['hokim', 'hyeokim', 'jiwonlee', 'jongkim', 'kwlee', 'minjakim', 'seolim', 'seomoon', 'snpark', 'sulee'],
+        ft_printf: ['hokim', 'hyeokim', 'jiwonlee', 'jongkim', 'kwlee', 'minjakim', 'seolim', 'seomoon', 'snpark', 'sulee'],
+        libasm: ['hokim', 'hyeokim', 'jiwonlee', 'jongkim', 'kwlee', 'minjakim', 'seolim', 'seomoon', 'snpark', 'sulee']
+    },
+    user: {
+        waitMatching: false
+    }
+};
 
 export default MatchingStateView;
