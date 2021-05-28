5ee8493850fb9abc3358558e1f5b03997760f06a
leejiwonn <bbongwa@naver.com>
2021-05-28 11:44:44 +0900

    fix: infinite scroll 스크롤 바 보이도록 수정

--- src/components/TeamListView/TeamListView.styles.js
+++ src/components/TeamListView/TeamListView.styles.js
@@ -4,7 +4,7 @@ export const TeamListViewStyled = styled.div`
   width: 100%;
   height: 78%;
   display: flex;
-	flex-direction: column;
+  flex-direction: column;
   align-items: start;
   background-color: #fff;
   padding: 30px;
@@ -13,36 +13,50 @@ export const TeamListViewStyled = styled.div`
 `;
 
 export const TeamListTopbarStyled = styled.div`
-	width: 100%;
-	max-width: inherit;
-	border-bottom: 2px solid rgba(0, 0, 0, 0.11);
-	padding-bottom: 20px;
-	display: flex;
-	justify-content: space-between;
+  width: 100%;
+  max-width: inherit;
+  border-bottom: 2px solid rgba(0, 0, 0, 0.11);
+  padding-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
 `;
 
-export const TeamListStyled = styled.div`
-
-`;
+export const TeamListStyled = styled.div``;
 
 export const TeamListTitleStyled = styled.span`
-	margin-left: 5px;
-	font-size: 1.25rem;
-	font-weight: bold;
+  margin-left: 5px;
+  font-size: 1.25rem;
+  font-weight: bold;
 `;
 
 export const TopbarBtnStyled = styled.div`
-	display: flex;
-	justify-content: space-between;
-	width: 30%;
-	padding: 0 5px;
-	font-size: 0.875rem;
+  display: flex;
+  justify-content: space-between;
+  width: 30%;
+  padding: 0 5px;
+  font-size: 0.875rem;
 `;
 
 export const TeamListContainerStyled = styled.div`
-	width: 100%;
-	overflow: scroll;
-	::hover {
-		overflow-y: scroll;
-	}
+  width: 100%;
+  overflow-y: scroll;
+  padding-right: 10px;
+  box-sizing: border-box;
+
+  ::-webkit-scrollbar {
+    display: block;
+    width: 4px;
+    padding-top: 10px;
+  }
+  ::-webkit-scrollbar,
+  ::-webkit-scrollbar-thumb {
+    overflow: visible;
+    border-radius: 4px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: #d5d5d5;
+  }
+  ::-webkit-scrollbar-track {
+    margin: 10px 0;
+  }
 `;
