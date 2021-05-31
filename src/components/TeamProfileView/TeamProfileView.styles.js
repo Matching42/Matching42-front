cd4fb84c35a34f342a20b1206eb6581f9e77b446
leejiwonn <bbongwa@naver.com>
2021-05-31 17:53:51 +0900

    feat: 상세 페이지 구조 설계 및 구현

---
+++ src/components/TeamProfileView/TeamProfileView.styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const TeamProfileViewStyled = styled.div`
+  width: 39%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  padding: 30px;
+  border-radius: 20px;
+  box-sizing: border-box;
+  font-size: 28px;
+`;
---
+++ src/components/TeamWorkspaceView/TeamWorkspaceView.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { TeamWorkspaceViewStyled } from './TeamWorkspaceView.styles';
+
+const TeamWorkspaceView = () => {
+  console.log('team workspace');
+  return <TeamWorkspaceViewStyled>Team Workspace</TeamWorkspaceViewStyled>;
+};
+
+export default TeamWorkspaceView;
