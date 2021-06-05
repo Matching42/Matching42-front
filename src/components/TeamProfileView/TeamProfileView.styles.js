b68ad5e3b1872855e43c1c59cd0cd24a2258222d
from97 <skm1104@daum.net>
2021-06-05 17:51:28 +0900

    feat: TeamProfileView 컴포넌트 스타일링

--- src/components/TeamProfileView/TeamProfileView.styles.js
+++ src/components/TeamProfileView/TeamProfileView.styles.js
@@ -4,25 +4,57 @@ export const TeamProfileViewStyled = styled.div`
   width: 39%;
   height: 100%;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column;
+  justify-content: space-around;
   background-color: #27babb;
   padding: 30px;
   border-radius: 20px;
   box-sizing: border-box;
   font-size: 28px;
+  color: #fff;
+  position: relative;
 `;
 
-export const TeamProfile = styled.div``;
+export const TeamProfile = styled.div`
+  display: flex;
+  font-size: 1em;
+`;
 
-TeamProfile.Name = styled.div``;
+TeamProfile.Name = styled.div`
+  font-size: 1.15em;
+  font-weight: bold;
+`;
 
 TeamProfile.EditButton = styled.div``;
 
-TeamProfile.Dday = styled.div``;
+TeamProfile.Dday = styled.div`
+  font-size: 1em;
+  font-weight: bold;
+  line-height: 1.3;
+  background-color: #219e9f;
+  border-radius: 8px;
+  box-sizing: border-box;
+  align-items: center;
+  text-align: center;
+  padding: 7px 15px;
+  position: absolute;
+  top: 34px;
+  right: 34px;
+`;
 
-export const TeamDescription = styled.div``;
+export const TeamDescription = styled.div`
+  font-size: 0.57em;
+  line-height: 1.2;
+`;
 
-export const TeamTagList = styled.div``;
+export const TeamTagList = styled.ul`
+  display: flex;
+  font-size: 0.61em;
+`;
 
-TeamTagList.Item = styled.div``;
+TeamTagList.Item = styled.li`
+  background-color: rgb(0, 0, 0, 0.15);
+  margin-right: 12px;
+  border-radius: 8px;
+  padding: 6px 12px;
+`;
