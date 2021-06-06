bc713e29fa5c8326bc7b162692a63f592ae68536
leejiwonn <bbongwa@naver.com>
2021-06-06 18:06:50 +0900

    feat: `Dialog(Modal)` 컴포넌트 구현 - `React Spectrum` 라이브러리 사용

---
+++ src/components/DialogCloseButton/DialogCloseButton.styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const CloseButton = styled.button`
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  top: 30px;
+  right: 30px;
+  cursor: pointer;
+  background-color: #fff;
+  outline: 0;
+
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+`;
