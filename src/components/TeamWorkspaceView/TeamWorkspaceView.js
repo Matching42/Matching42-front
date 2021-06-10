07bf5beaca728c62c7044e9813874df1a618ac10
0000000000ps <sulee@student.42seoul.kr>
2021-06-10 23:52:23 +0900

    feat: 컴포넌트 추가 및 정렬

--- src/components/TeamWorkspaceView/TeamWorkspaceView.js
+++ src/components/TeamWorkspaceView/TeamWorkspaceView.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import { TeamWorkspaceViewStyled } from './TeamWorkspaceView.styles';
+import { TeamWorkspaceViewStyled, Line, LinkList, FinishStudyButton } from './TeamWorkspaceView.styles';
 
 const TeamWorkspaceView = () => {
   console.log('team workspace');
-  return <TeamWorkspaceViewStyled>Team Workspace</TeamWorkspaceViewStyled>;
+  return (
+    <TeamWorkspaceViewStyled>
+      <p>
+        <TeamWorkspaceViewStyled.Title>Team Workspace Link</TeamWorkspaceViewStyled.Title>
+        <TeamWorkspaceViewStyled.Line />
+        <LinkList>
+          <LinkList.Title>GitHub Repository</LinkList.Title>
+          <LinkList.Link>https://github.com/Matching42/Matching42-front</LinkList.Link>
+          <Line />
+          <LinkList.Title>Notion</LinkList.Title>
+          <LinkList.Link>
+            <a href="https://github.com/Matching42/Matching42-front">https://github.com/Matching42/Matching42-front</a>
+          </LinkList.Link>
+          <Line />
+          <LinkList.Title>Subject PDF</LinkList.Title>
+          <LinkList.Link>
+            <a href="https://github.com/Matching42/Matching42-front">https://github.com/Matching42/Matching42-front</a>
+          </LinkList.Link>
+          <Line />
+        </LinkList>
+        <FinishStudyButton>스터디 종료하기</FinishStudyButton>
+      </p>
+    </TeamWorkspaceViewStyled>
+  );
 };
 
 export default TeamWorkspaceView;
--- src/components/TeamWorkspaceView/TeamWorkspaceView.styles.js
+++ src/components/TeamWorkspaceView/TeamWorkspaceView.styles.js
@@ -4,11 +4,62 @@ export const TeamWorkspaceViewStyled = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: top;
+  align-items: top;
   background-color: #fff;
   padding: 30px;
   border-radius: 20px;
   box-sizing: border-box;
   font-size: 28px;
+`;
+
+TeamWorkspaceViewStyled.Title = styled.div`
+  font-family: 'Noto Sans KR', sans-serif;
+  font-weight: bold;
+  font-size: 1rem;
+  margin-bottom: 10px;
+`;
+
+TeamWorkspaceViewStyled.Line = styled.div`
+  border: solid 2px #f9f9f9;
+`;
+
+export const Line = styled.div`
+  border: solid 1px #f9f9f9;
+  width: 1090px;
+  border-bottom: 0px;
+  text-align: left;
+`;
+
+export const LinkList = styled.div`
+  font-size: 0.75rem;
+  vertical-align: top;
+`;
+
+LinkList.Title = styled.div`
+  font-size: 0.75rem;
+  font-weight: bold;
+  margin-top: 20px;
+  margin-left: 6px;
+`;
+
+LinkList.Link = styled.div`
+  color: #d3d4d6;
+  margin-top: 9px;
+  margin-left: 6px;
+  margin-bottom: 18px;
+`;
+
+export const FinishStudyButton = styled.div`
+  width: 6rem;
+  text-align: center;
+  font-size: 0.8rem;
+  background: #0bd;
+  color: #fff;
+  background: #27babb;
+  border-radius: 5px;
+  margin-top: 2.5rem;
+  margin-left: 61rem;
+  padding: 0.9rem;
+  cursor: pointer;
 `;
