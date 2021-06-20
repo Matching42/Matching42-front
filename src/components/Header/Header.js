eb86b663592aed8777c0a80736cd76daf8a3a03a
deathnet <53890887+deathnet@users.noreply.github.com>
2021-06-20 22:16:10 +0900

    feature: 로그아웃 UI 구현
    - Header UserImage 클릭 시 로그아웃 UI 출력
    
    resolved:#73

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import HeaderStyled from './Header.styles';
+import HeaderStyled, { Logout } from './Header.styles';
 import { ReactComponent as Logo } from '../../assets/images/42_logo.svg';
 import { UserImageStyled } from '../TeamListItem/common/TeamImage/TeamImage.styles';
 
 const Header = props => {
   const { user } = props;
+	const [logState, setlogState] = useState(false);
+
+	const logoutWindowManage = () => {
+		setlogState(!logState);
+	};
+
+	const FuncLogout = () => {
+		console.log("Logout");
+	}
 
   return (
-    <HeaderStyled>
-      <div className="logo">
-        <Link to="/">
-          <Logo />
-        </Link>
-      </div>
-      {user && <UserImageStyled key={1} size="big" url={`https://cdn.intra.42.fr/users/small_${user.nickname}.jpg`} />}
-    </HeaderStyled>
+		<HeaderStyled>
+			<div className="logo">
+				<Link to="/">
+					<Logo />
+				</Link>
+			</div>
+			<HeaderStyled.Image>
+				{user && <UserImageStyled onClick={logoutWindowManage} key={1} size="big" url={`https://cdn.intra.42.fr/users/small_${user.nickname}.jpg`} />}
+			</HeaderStyled.Image>
+			<Logout logState={logState}>
+				<div className="bubble">
+					<Logout.UserWrap>
+						<UserImageStyled size="big" url={`https://cdn.intra.42.fr/users/small_${user.nickname}.jpg`}/>
+						<Logout.UserInfo>
+							<h2>{ user.nickname }</h2>
+							<p>
+								Level 2 - 30% | {user.blackhole} days left
+							</p>
+						</Logout.UserInfo>
+					</Logout.UserWrap>
+					<Logout.Button onClick={FuncLogout}>로그아웃</Logout.Button>
+				</div>
+			</Logout>
+		</HeaderStyled>
   );
 };
 
