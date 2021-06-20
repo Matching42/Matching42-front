d739c13852ce7d2b0005cb8228a58a8cf1c1299e
leejiwonn <bbongwa@naver.com>
2021-06-20 16:13:20 +0900

    fix: `Dropdown` 아이콘 컴포넌트 명 변경

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { DropdownStyled, DropdownButton, DropdownBox } from './Dropdown.styles';
-import SVG from '../../assets/icons/SVG';
+import DropdownIcon from '../../assets/icons/DropdownIcon';
 
 const Dropdown = ({ subjectList }) => {
   const [isActive, setIsActive] = useState(false);
@@ -20,7 +20,7 @@ const Dropdown = ({ subjectList }) => {
       <DropdownButton type="button" onClick={buttonOnClick} active={isActive}>
         <DropdownButton.Name>{selectedSubject}</DropdownButton.Name>
         <DropdownButton.ArrowIcon>
-          <SVG color={isActive ? '#27BABB' : '#252831'} active={isActive ? 'translate(1453.069 137.659) rotate(180)' : 'translate(-1439.141 -128.991)'} />
+          <DropdownIcon color={isActive ? '#27BABB' : '#252831'} active={isActive ? 'translate(1453.069 137.659) rotate(180)' : 'translate(-1439.141 -128.991)'} />
         </DropdownButton.ArrowIcon>
       </DropdownButton>
       <DropdownBox active={isActive}>
