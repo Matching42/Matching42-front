82f2961f6d4cf9ce306d2ebf4dff9d6f48429138
deathnet <53890887+deathnet@users.noreply.github.com>
2021-07-24 02:25:32 +0900

    fix: MyTeam 이 없을 때 로딩이 계속 도는 문제 수정

--- src/components/MyTeamListView/MyTeamListView.js
+++ src/components/MyTeamListView/MyTeamListView.js
@@ -7,19 +7,17 @@ import LoaderSpinner from '../LoaderSpinner/LoaderSpinner';
 const MyTeamListView = ({ myTeamList }) => {
   const { getTeamData } = useTeamData(myTeamList);
 
-  if (getTeamData.data === null || getTeamData.data?.data === null || getTeamData.data?.data === undefined) {
-    return (
-      <MyTeamListViewStyled>
-        <Topbar>My Team</Topbar>
-        <LoaderSpinner />
-      </MyTeamListViewStyled>
-    );
-  }
+  const checkLoading = () => {
+    const isLoading = getTeamData.data?.data === undefined;
+
+    if (isLoading) return <LoaderSpinner />;
+    return getTeamData.data.data ? <TeamListItem teamData={getTeamData.data.data} /> : <NoneMyTeamData>아직 참여중인 팀이 없습니다.</NoneMyTeamData>;
+  };
 
   return (
     <MyTeamListViewStyled>
       <Topbar>My Team</Topbar>
-      {getTeamData.data.data ? <TeamListItem teamData={getTeamData.data.data} /> : <NoneMyTeamData>아직 참여중인 팀이 없습니다.</NoneMyTeamData>}
+      {checkLoading()}
     </MyTeamListViewStyled>
   );
 };
