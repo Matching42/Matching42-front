f032d4b787f837ab899edc726f9ac849cba470f5
from97 <skm1104@daum.net>
2021-12-11 11:21:02 +0900

    login url dev server로 변경

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,7 +3,7 @@ import LoginSectionView from '../components/LoginSectionView/LoginSectionView';
 
 const LoginPage = () => {
   const handleLoginButtonClick = () => {
-    window.location.replace('https://matching42-server.cf/login/42');
+    window.location.replace('https://matching42-dev-server.cf/login/42');
   };
 
   return <LoginSectionView onLoginButtonClick={handleLoginButtonClick} />;
